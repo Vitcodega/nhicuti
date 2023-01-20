6d88b5d8ad62ab9884a0d760015ef69882cf958e
Vitcodega <122954410+Vitcodega@users.noreply.github.com>
2023-01-20 11:59:53 +0700

    UPLOAD

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const b = document.querySelector('button.button--no')
-        b.addEventListener("mouseover", moveHover)
+var fut = new Date("jan 12, 2023 13:29:00").getTime()
+let x = setInterval(function stime(){
+    var now = new Date().getTime()
+    var D = fut - now
+    var days = Math.floor(D/(1000*60*60*24))
+    var hours = Math.floor(D/(1000*60*60))
+    var minutes = Math.floor(D/(1000*60))
+    var seconds = Math.floor(D/(1000))
+    hours %=24
+    minutes %=60
+    seconds %=60
+    document.getElementById("days").innerText = days
+    document.getElementById("hours").innerText = hours
+    document.getElementById("minutes").innerText = minutes
+    document.getElementById("seconds").innerText = seconds
 
-        function moveHover(){
-            const i = Math.floor(Math.random()*500)+1 ;
-            const j = Math.floor(Math.random()*500)+1 ;
-
-            b.style.left=i+"px";
-            b.style.top=j+"px";
-        }
-        let btnyes = document.querySelector('.button--yes');
-        let mail = document.querySelector('.mail');
-        let text1 = document.querySelector('.text1');
-        let text2 = document.querySelector('.text2');
-        let yes = document.querySelector('#yes');
-        let no = document.querySelector('#no');
-        
-        btnyes.onclick = function(){
-            mail.classList.toggle('active')
-            text1.classList.toggle('active')
-            text2.classList.toggle('active')
-            yes.classList.toggle('active')
-            no.classList.toggle('active')
-        }
+    if(D<0){
+        clearInterval(x);
+        window.location.href="intro.html"
+    }
+}, 0.1);
